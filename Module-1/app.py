5fce594f20f7ef2f2cbb9604bd85df4a7a3b8ee1
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-10-27 12:22:26 -0400

    saving progress

--- Module-1/app.py
+++ Module-1/app.py
@@ -5,6 +5,7 @@
 
  #import the flask library
 from flask import Flask
+import random 
 #set our app variable to write routes
 app = Flask(__name__)
 
@@ -26,6 +27,30 @@ def favorite_dessert(users_dessert):
 @app.route('/madlibs/<adjective>/<noun>')
 def madlibs(adjective, noun):
     return f'We went camping and ran into a {adjective} {noun}.'
+
+@app.route('/multiply/<number1>/<number2>')
+def multiply_2_numbers(number1, number2):
+    if (type(number1) == int and type(number2) == int ):
+        total = number1 + number2
+        return f'{number1} times {number2} is {total}'
+    else:
+        return 'Please try again by entering 2 numbers!'
+
+@app.route('/sayntimes/<word>/<n>') 
+def say_N_times(word, n):
+    if(type(n) == int):
+        return f'{word}' * n
+    else:
+        return 'Invalid input. Please try again by entering a word and a number!'
+
+
+@app.route('/dicegame')
+def dice_game():
+    number = random.randint(0,6)
+    if (number == 6):
+        return f' you rolled a 6. You won!'
+    else:
+        return f'You rolled a {number}. You lost!'
 
 
 if __name__ == '__main__':
