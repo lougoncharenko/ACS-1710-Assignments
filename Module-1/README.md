ea5c9b6cf2e805126c63a825e34c556d5959c9a2
Louisa Goncharenko <louisa.gonch@gmail.com>
2022-10-26 20:57:30 -0400

    saving progress

--- Module-1/README.md
+++ Module-1/README.md
@@ -2,7 +2,17 @@
 
 ## Usage
 
-run `pip3 install pytest `
+run `pip3 install pytest ` for test_app.py
+
+run `pip3 install flask` for app.py
+
+run `python3 app.py`
+
+## Components of program
+### Hello, World
+A program that imports a flask library and creates a route that returns a greeting to the user.
+
+### Your User's Favorite Animal
 
 ## Purpose of this project
 
--- Module-1/app.py
+++ Module-1/app.py
@@ -1,2 +1,32 @@
 # TODO: Follow the assignment instructions to complete the required routes!
 # (And make sure to delete this TODO message when you're done!)
+
+#**************** Hello World **********************
+
+ #import the flask library
+from flask import Flask
+#set our app variable to write routes
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return 'Hello user! Welcome to the homepage!'
+
+#**************** Favorite Animal **********************
+@app.route('/animal/<users_animal>')
+def favorite_animal(users_animal):
+    return f'{users_animal} is an awesome animal!'
+
+#**************** Favorite Dessert**********************
+@app.route('/dessert/<users_dessert>')
+def favorite_dessert(users_dessert):
+    return f'{users_dessert} sounds delicious'
+
+
+@app.route('/madlibs/<adjective>/<noun>')
+def madlibs(adjective, noun):
+    return f'We went camping and ran into a {adjective} {noun}.'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
